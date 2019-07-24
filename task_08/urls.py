530cb474e349d79c504034e281ee036b967204c0
Walaa Alrefaee <walaaalrefaee4@gmail.com>
2019-07-24 19:06:15 +0300

    complete task_08

--- task_08/urls.py
+++ task_08/urls.py
@@ -29,6 +29,6 @@ urlpatterns = [
     path('restaurants/update/<int:restaurant_id>/',views.restaurant_update ,name='restaurant-update'),
     path('restaurants/delete/<int:restaurant_id>/',views.restaurant_delete ,name='restaurant-delete'),
 ]
-
+urlpatterns+=static( settings.MEDIA_URL,document_root=settings.MEDIA_ROOT )
 if settings.DEBUG:
-    urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT )
